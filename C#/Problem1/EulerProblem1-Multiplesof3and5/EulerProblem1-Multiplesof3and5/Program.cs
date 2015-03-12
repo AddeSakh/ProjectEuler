6609f7de860ad5f61c74b58bfccd486ba99579d5
unknown <adikha@adikha-t440p.iteroab.local>
2015-03-12 11:23:49 +0100

    First solution - Bruteforce

---
+++ C#/Problem1/EulerProblem1-Multiplesof3and5/EulerProblem1-Multiplesof3and5/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EulerProblem1_Multiplesof3and5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
