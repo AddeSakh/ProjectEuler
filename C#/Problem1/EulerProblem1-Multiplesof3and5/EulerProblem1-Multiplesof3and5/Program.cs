0fee1a49ad5ddfc239f4138ba7168493a6f07a30
unknown <adikha@adikha-t440p.iteroab.local>
2015-03-12 12:51:41 +0100

    Update with comments

--- C#/Problem1/EulerProblem1-Multiplesof3and5/EulerProblem1-Multiplesof3and5/Program.cs
+++ C#/Problem1/EulerProblem1-Multiplesof3and5/EulerProblem1-Multiplesof3and5/Program.cs
@@ -10,6 +10,18 @@ namespace EulerProblem1_Multiplesof3and5
     {
         static void Main(string[] args)
         {
+            int sum = 0;
+            //Going through all numbers untill upper range i.e. 1000
+            //Thats Bruteforce solution
+            for (int i = 0; i < 1000; i++)
+            {
+                if ((i%3==0) || (i%5==0)) //Checking if its multiple of 3 or 5
+                {
+                    sum += i; //If true, add to sum initiated by zero
+                }
+            }
+            Console.WriteLine(sum); //Print the calculated result
+            Console.Read();
         }
     }
 }
